8cc8dc439348faf3e36309a6b4e6a3b9e9f8c980
Dev77 <manhnv.dotnet@gmail.com>
2022-08-22 03:04:03 +0700

    Create project

---
+++ Nippo/Nippo/Views/PageView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nippo.Views
+{
+    /// <summary>
+    /// Interaction logic for PageView.xaml
+    /// </summary>
+    public partial class PageView : UserControl
+    {
+        public PageView()
+        {
+            InitializeComponent();
+            PagesNavigation.Navigate(new System.Uri("Views/Home.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void rdHome_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Views/Home.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void rdUsers_Click(object sender, RoutedEventArgs e)
+        {
+            // PagesNavigation.Navigate(new HomePage());
+
+            PagesNavigation.Navigate(new System.Uri("Views/User.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
